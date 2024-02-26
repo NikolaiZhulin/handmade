af02719511d9c35d2159329d2b9f2caec24f2cb6
NikolayZhulin <nikolayzhulin97@mail.ru>
2024-02-26 23:35:39 +0100

    update

--- src/containers/Modal/components/BodyPortal.tsx
+++ src/containers/Modal/components/BodyPortal.tsx
@@ -10,5 +10,5 @@ export default function BodyPortal({ children }: {children: ReactNode}) {
 
   if (typeof window === 'undefined') return null;
 
-  return <>{isMounted && createPortal(children, document.body)}</>;
+  return <>{isMounted && createPortal(children as ReactPortal, document.body)}</>;
 }
