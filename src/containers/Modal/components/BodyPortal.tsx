cdd16d9dc5eb715e3f72634ce6a8bdcb2b7c0ff7
NikolayZhulin <nikolayzhulin97@mail.ru>
2024-02-26 23:46:22 +0100

    update

--- src/containers/Modal/components/BodyPortal.tsx
+++ src/containers/Modal/components/BodyPortal.tsx
@@ -1,12 +1,9 @@
-import {ReactNode, ReactPortal, useState} from 'react';
+import {ReactNode, ReactPortal} from 'react';
 import { createPortal } from 'react-dom';
 
-import useSSRLayoutEffect from '@/hooks/common/useSSRLayoutEffect';
 
-export default function BodyPortal({ children }: {children: ReactNode}): null | ReactPortal {
-  const [isMounted, setIsMounted] = useState(false);
+export default function BodyPortal({ children }: {children: ReactNode}) {
+  if (typeof window === 'undefined') return null;
 
-  useSSRLayoutEffect(() => setIsMounted(true), []);
-
-  return isMounted ? createPortal(children as ReactNode, document.body) : null;
+  return createPortal(children as ReactNode, document.body);
 }
