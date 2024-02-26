81501a6dffceacc8dc60d5093baadbbc6f6a33fe
NikolayZhulin <nikolayzhulin97@mail.ru>
2024-02-26 23:49:48 +0100

    update

--- src/containers/Modal/components/BodyPortal.tsx
+++ src/containers/Modal/components/BodyPortal.tsx
@@ -1,9 +1,14 @@
-import {ReactNode, ReactPortal} from 'react';
+import {ReactNode, useEffect, useState} from 'react';
 import { createPortal } from 'react-dom';
 
 
-export default function BodyPortal({ children }: {children: ReactNode}) {
-  if (typeof window === 'undefined') return null;
 
-  return createPortal(children as ReactNode, document.body);
+export default function BodyPortal({ children }: { children: ReactNode }) {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => setIsMounted(true), []);
+
+  if (typeof window === 'undefined' || !isMounted) return null;
+
+  return createPortal(children, document.body);
 }
