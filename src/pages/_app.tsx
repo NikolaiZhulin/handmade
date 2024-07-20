c59fd68b4e5f84e813ee92ab74853402a03b546a
Anton Krusin <krusinanton@gmail.com>
2024-07-20 22:50:19 +0300

    add i18n.js

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -24,6 +24,7 @@ import { useModalContext } from '@/hooks/useModalContext/useModalContext';
 import { SOCIAL_AUTH_CHANEL } from '@/constants/base';
 
 import nextI18NextConfig from '../../next-i18next.config';
+// import '../../i18n';
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
